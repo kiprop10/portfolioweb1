d5008a2f6a7f5bc77af26c333c5f03e7df561719
ezramutai <157595638+ezramutai@users.noreply.github.com>
2025-05-18 14:31:27 +0000

    commiting practice ng files

---
+++ portfolio/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'nav', component: NavComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'portfolio', component: PortfolioComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'contact', component: ContactComponent }
+];
+/*
+No, you do not need to import RouterLink or RouterLinkActive in this routes configuration file.
+Those are directives used in your component templates (HTML), not in the routing configuration.
+*/
