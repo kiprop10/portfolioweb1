96dcebabc17a928eed4f086203068b4a29efe933
ezramutai <157595638+ezramutai@users.noreply.github.com>
2025-05-19 20:09:15 +0000

    added a notfound page component with working suggestion link to homepage

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { HomeComponent } from './components/home/home.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { ServicesComponent } from './components/services/services.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'portfolio', component: PortfolioComponent },
     { path: 'services', component: ServicesComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path:"**", component: NotfoundComponent}
+
 ];
 /*
 No, you do not need to import RouterLink or RouterLinkActive in this routes configuration file.
