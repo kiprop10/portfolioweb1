151b39d7f7188e62d631cdeaa5470d21d3a3c973
ezramutai <157595638+ezramutai@users.noreply.github.com>
2025-05-23 17:48:52 +0000

    added assets/images folder, added some content to my homepage(homecomponent)

--- portfolio/src/app/components/home/home.component.ts
+++ portfolio/src/app/components/home/home.component.ts
@@ -9,5 +9,12 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+name = 'Kiprop Mutai';
+description = 'Web Developer 	and designer';
+intro = 'Welcome to my portfolio website! I build modern responsive websites and web applications using angular and other technologies. I am passionate about creating user-friendly and visually appealing digital experiences. My goal is to help businesseses and individuals establisha strong online presence and achieve their goals through effective web solutions. Whether you need a simple website or a complex web application, I am here to help you bring yourvision to life. Let us work together to create something amazing!';
+skills = [
+  {name: 'HTML', icon: 'fa-brands fa-html5'},
+  {name: 'css', icon: 'fa-brands fa-css3-alt'},
+];
+profileimage = 'assets/images/profile1.jpg';
 }
