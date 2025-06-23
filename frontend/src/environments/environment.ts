23c7143ca01b7d5653d207ee770f3e4b6bd8f55c
kiprop10 <kipropmutai10@gmail.com>
2025-06-23 10:13:37 +0000

    changing my server side url in environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://supreme-parakeet-x54rg4r9pjxr2vpvx-3000.app.github.dev'
+  apiUrl: 'https://portfolioweb1-g89s.onrender.com'
 };
