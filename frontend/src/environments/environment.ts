a52b49bafcd90d61a589b609efa2fea06b6860d0
kiprop10 <kipropmutai10@gmail.com>
2025-06-28 13:55:03 +0000

    readded environment.ts

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+    apiUrl: 'https://portfolioweb1-g89s.onrender.com' // change to your actual backend URL
+    };
