3106b3a90941f40dd8d9757e1b175f370a61ac44
kiprop10 <kipropmutai10@gmail.com>
2025-06-29 11:02:01 +0000

    now able to type in links and image refs into blog updated formatting for blog(paragraphing, e.t.c)

--- frontend/src/app/components/blog/blog.component.ts
+++ frontend/src/app/components/blog/blog.component.ts
@@ -6,67 +6,87 @@ import { environment } from '../../../environments/environment';
 
 interface BlogComment {
   _id?: string;
-    author: string;
-      text: string;
-        createdAt?: string;
-        }
+  author: string;
+  text: string;
+  createdAt?: string;
+}
 
-        interface Blog {
-          _id: string;
-            title: string;
-              imageUrl: string;
-                content: string;
-                  comments: BlogComment[];
-                    createdAt?: string;
-                      expanded?: boolean;
-                        newCommentAuthor?: string;
-                          newCommentText?: string;
-                          }
+interface Blog {
+  _id: string;
+  title: string;
+  imageUrl: string;
+  content: string;
+  comments: BlogComment[];
+  createdAt?: string;
+  expanded?: boolean;
+  newCommentAuthor?: string;
+  newCommentText?: string;
+}
 
-                          @Component({
-                            selector: 'app-blog',
-                              standalone: true,
-                                imports: [CommonModule, FormsModule, HttpClientModule],
-                                  templateUrl: './blog.component.html',
-                                    styleUrls: ['./blog.component.css']
-                                    })
-                                    export class BlogComponent implements OnInit {
-                                      blogs: Blog[] = [];
-                                        public apiUrl = environment.apiUrl;
+@Component({
+  selector: 'app-blog',
+  standalone: true,
+  imports: [CommonModule, FormsModule, HttpClientModule],
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css']
+})
+export class BlogComponent implements OnInit {
+  blogs: Blog[] = [];
+  public apiUrl = environment.apiUrl;
 
-                                          constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-                                            ngOnInit(): void {
-                                                this.loadBlogs();
-                                                  }
+  ngOnInit(): void {
+    this.loadBlogs();
+  }
 
-                                                      loadBlogs() {
-                                                          this.http.get<Blog[]>(`${this.apiUrl}/api/blogs`).subscribe(blogs => {
-                                                                this.blogs = blogs.map(blog => ({
-                                                                        ...blog,
-                                                                                imageUrl: blog.imageUrl ? this.apiUrl + blog.imageUrl : ''
-                                                                                      }));
-                                                                                          });
-                                                                                            }
+  loadBlogs() {
+    this.http.get<Blog[]>(`${this.apiUrl}/api/blogs`).subscribe(blogs => {
+      this.blogs = blogs.map(blog => ({
+        ...blog,
+        imageUrl: blog.imageUrl ? this.apiUrl + blog.imageUrl : ''
+      }));
+    });
+  }
 
-                                                                                              showDetail(blog: Blog) {
-                                                                                                  this.blogs.forEach(b => b.expanded = false);
-                                                                                                      blog.expanded = true;
-                                                                                                        }
+  showDetail(blog: Blog) {
+    this.blogs.forEach(b => b.expanded = false);
+    blog.expanded = true;
+  }
 
-                                                                                                          closeDetail(blog: Blog) {
-                                                                                                              blog.expanded = false;
-                                                                                                                }
+  closeDetail(blog: Blog) {
+    blog.expanded = false;
+  }
 
-                                                                                                                  addComment(blog: Blog) {
-                                                                                                                      if (!blog.newCommentAuthor || !blog.newCommentText) return;
-                                                                                                                          this.http.post(`${this.apiUrl}/api/blogs/${blog._id}/comments`, {
-                                                                                                                                author: blog.newCommentAuthor,
-                                                                                                                                      text: blog.newCommentText
-                                                                                                                                          }).subscribe((updatedBlog: any) => {
-                                                                                                                                                blog.comments = updatedBlog.comments;
-                                                                                                                                                      blog.newCommentAuthor = '';
-                                                                                                                                                            blog.newCommentText = '';
-                                                                                                                                                                });
-                                                                                                                                                                }
-                                                                                                                                                                }
+  addComment(blog: Blog) {
+    if (!blog.newCommentAuthor || !blog.newCommentText) return;
+    this.http.post(`${this.apiUrl}/api/blogs/${blog._id}/comments`, {
+      author: blog.newCommentAuthor,
+      text: blog.newCommentText
+    }).subscribe((updatedBlog: any) => {
+      blog.comments = updatedBlog.comments;
+      blog.newCommentAuthor = '';
+      blog.newCommentText = '';
+    });
+  }
+
+  // ✅ Format blog content to render images, links, and paragraphs
+  formatContent(content: string): string {
+    if (!content) return '';
+
+    let formatted = content
+      // Inline image syntax: [img:/url|caption]
+      .replace(/img:(.*?)\|(.*?)/g, (_, url, caption) => `
+        <figure>
+          <img src="${this.apiUrl + url}" alt="${caption}" />
+          <figcaption>${caption}</figcaption>
+        </figure>
+      `)
+      // Convert links to anchor tags
+      .replace(/(https?:\/\/[^\s<]+)/g, url => `<a href="${url}" target="_blank" rel="noopener noreferrer">${url}</a>`)
+      // Replace line breaks with <br>
+      .replace(/\n/g, '<br>');
+
+    return formatted;
+  }
+}
